d1f9753810d538c79b76acd571f78835f15959b5
Sergey Babitsky <babitsky.sergey@gmail.com>
2014-07-18 13:58:59 +0300

    Moving to TypeScript

--- repository/group.js
+++ repository/group.js
@@ -1,40 +1,30 @@
-﻿var Base = require('./base'),
-    Group = require('../models/group'),
-    Permission = require('../models/permission');
+﻿/// <reference path="../compiler/bi.d.ts" />
+var Base = require('./base'), Group = require('../models/group'), Permission = require('../models/permission');
 
-var GroupRepository = Base.extend(function () { })
-    .methods({
-        getByName: function(name, done) {
-            ///<summary>Gets group by name</summary>
-            ///<param name="id">Group name</param>
-            ///<param name="done">Done callback</param>
+var GroupRepository = (function () {
+    function GroupRepository() {
+    }
+    GroupRepository.prototype.getByName = function (name, done) {
+        return new GroupRepository().getByNames([name], done);
+    };
 
-            return new GroupRepository().getByNames([name], done);
-        },
+    GroupRepository.prototype.getByNames = function (names, done) {
+        return Group.findOne({
+            name: {
+                $in: names
+            }
+        }, done);
+    };
 
-        getByNames: function(names, done) {
-            ///<summary>Gets groups by names</summary>
-            ///<param name="id">Group names</param>
-            ///<param name="done">Done callback</param>
-
-            return Group.findOne({
-                name: {
-                    $in: names
-                }
-            }, done);
-        },
-
-        getByIds: function(ids, done) {
-            ///<summary>Gets groups by ids</summary>
-            ///<param name="ids">collection of identifiers</param>
-            ///<param name="done">Done callback</param>
-
-            return Group.find({
-                _id: {
-                    $in: ids
-                }
-            }, done);
-        }
-    });
+    GroupRepository.prototype.getByIds = function (ids, done) {
+        return Group.find({
+            _id: {
+                $in: ids
+            }
+        }, done);
+    };
+    return GroupRepository;
+})();
 
 module.exports = GroupRepository;
+//# sourceMappingURL=group.js.map
---
+++ repository/group.ts
@@ -0,0 +1,31 @@
+﻿/// <reference path="../compiler/bi.d.ts" />
+var Base = require('./base'),
+    Group = require('../models/group'),
+    Permission = require('../models/permission');
+
+class GroupRepository {
+    constructor() {
+    }
+
+    getByName(name, done: Function) {
+        return new GroupRepository().getByNames([name], done);
+    }
+
+    getByNames(names, done) {
+        return Group.findOne({
+            name: {
+                $in: names
+            }
+        }, done);
+    }
+
+    getByIds(ids, done: Function) {
+        return Group.find({
+            _id: {
+                $in: ids
+            }
+        }, done);
+    }
+}
+
+export = GroupRepository;
